673668d241ba5318051e6fdb9382fccc34339739
ChaseWink05 <155601183+ChaseWink05@users.noreply.github.com>
2025-03-07 22:37:39 -0500

    created all the different files for the different users

---
+++ nsae-dashboard/src/components/Volunteers.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Volunteers() {
+    return (
+        <div>
+        <h1>Volunteers Page</h1>
+        <p>Welcome, Volunteers! Here you can view reports and manage the organization.</p>
+        </div>
+    );
+    }
+    export default Volunteers;
