a36af87cae2e494e27a5ef849fad30e4f9b505dd
ChaseWink05 <155601183+ChaseWink05@users.noreply.github.com>
2025-03-07 22:19:40 -0500

    Adding images, and readjusting nav bar

--- nsae-dashboard/src/components/Navbar.jsx
+++ nsae-dashboard/src/components/Navbar.jsx
@@ -24,7 +24,7 @@ function Navbar() {
         </Link>
       </div>
       <ul className="navbar-links">
-        <li><Link to="/">Home</Link></li>
+        <li><Link to="/VolunteerSignup">Volunteer Now!</Link></li>
       </ul>
     </nav>
   );
