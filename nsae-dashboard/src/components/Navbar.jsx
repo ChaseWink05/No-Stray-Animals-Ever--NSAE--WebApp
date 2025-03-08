600e84cd371e1481936bbc4c9cccb1dc1610657e
ChaseWink05 <155601183+ChaseWink05@users.noreply.github.com>
2025-03-07 21:34:19 -0500

    Created a hamburger-menu

--- nsae-dashboard/src/components/Navbar.jsx
+++ nsae-dashboard/src/components/Navbar.jsx
@@ -6,16 +6,18 @@ import logo from "../Images/Logo.jpg"; // Import the image
 function Navbar() {
   return (
     <nav className="navbar">
-      <ul className="navbar-links">
-        <li className="dropdown">
-          <span>Menu</span>
-          <div className="dropdown-content">
-            <Link to="/about">About Us</Link>
-            <Link to="/contact">Contact</Link>
-            <Link to="/volunteer">Volunteer</Link>
-          </div>
-        </li>
-      </ul>
+      <div className="hamburger-menu">
+        <div className="hamburger-icon">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="dropdown-content">
+          <Link to="/about">About Us</Link>
+          <Link to="/contact">Contact</Link>
+          <Link to="/volunteer">Volunteer</Link>
+        </div>
+      </div>
       <div className="navbar-logo">
         <img src={logo} alt="NSAE Logo" /> {/* Use the imported image */}
       </div>
